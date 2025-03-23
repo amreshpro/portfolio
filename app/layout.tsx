9728ba2e972002a7885ad4c0f845fe1158272882
Amresh Maurya <amresh.terminal@gmail.com>
2025-03-23 23:50:55 +0530

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       <body className={` p-5`} >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
