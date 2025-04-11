cb07573b267c2ff3ff24b317fc5b9a9800253da4
Amresh Maurya <amresh.terminal@gmail.com>
2025-04-11 10:29:44 +0530

    add social-media links

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,33 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import SocialMediaLinks from "../../constants/SocialMediaLinks";
+
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="py-6 mt-12 bg-[var(--primary)]">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row justify-between items-center gap-4">
+        <p className="text-xl text-center md:text-left">
+          © {new Date().getFullYear()} Amresh Maurya. All rights reserved.
+        </p>
+
+        <div className="flex gap-4 text-2xl">
+          <a
+            href={SocialMediaLinks.github}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-colors "
+          >
+            <FaGithub />
+          </a>
+          <a
+            href={SocialMediaLinks.linkedin}
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" transition-colors"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
---
+++ src/constants/SocialMediaLinks.tsx
@@ -0,0 +1,7 @@
+export default {
+  linkedin: "https://linkedin.com/in/amreshpro",
+  github: "https://github.com/amreshpro",
+  portfolio: "https://amreshpro.com",
+  hackerrank: "https://hackerrank.com/amreshpro",
+  leetcode: "https://leetcode.com/amreshpro",
+};
