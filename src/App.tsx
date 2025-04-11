f3209074238bf1a80a9b2bfa3718d9fdb905fa40
Amresh Maurya <amresh.terminal@gmail.com>
2025-04-11 17:19:11 +0530

    add toaster and .env-example

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Skills from "./components/skills";
 import { ThemeProvider } from "./provider/ThemeProvider";
 import Profiles from "./components/profiles";
 import Hero from "./components/hero";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
@@ -16,15 +17,42 @@ export default function App() {
       <ThemeProvider>
         <Navbar />
         <main className="py-20 px-4 flex flex-col gap-16 items-center ">
-          <Hero/>
+          <Hero />
           <Skills />
           <Projects />
-          <Profiles/>
+          <Profiles />
           <Blogs />
           <About />
           <Contact />
         </main>
         <Footer />
+
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          gutter={8}
+          containerClassName=""
+          containerStyle={{}}
+          toastOptions={{
+            // Define default options
+            className: "",
+            duration: 5000,
+            removeDelay: 1000,
+            style: {
+              background: "#363636",
+              color: "#fff",
+            },
+
+            // Default options for specific types
+            success: {
+              duration: 3000,
+              iconTheme: {
+                primary: "green",
+                secondary: "black",
+              },
+            },
+          }}
+        />
       </ThemeProvider>
     </BrowserRouter>
   );
